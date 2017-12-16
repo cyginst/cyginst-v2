3bcfbce9522b4e6155716e27f209d671dba6f3dc
root <root@super-computer>
2017-12-16 19:13:54 +0900

    @

---
+++ _boot.bat
@@ -0,0 +1,55 @@
+@if(0)==(0) echo off
+setlocal
+
+set CYG_NAME=project1
+set CYG_BITS=32
+::set CYG_CATS=Archive,Python
+set CYG_PKGS=procps,psmisc,tmux,vim,emacs-w32,git,dos2unix,gcc-g++,make
+::set DT_ICONS=1
+set CYG_HOME=.$
+
+set CYG_DEBUG=0
+set CYG_SITE=http://mirrors.kernel.org/sourceware/cygwin/
+set CYG_LANG=ja
+set CYG_FONT=MS Gothic
+set CYG_FONT_HEIGHT=12
+set CYG_CURSOR_TYPE=block
+set CYG_CONFIRM_EXIT=no
+
+::rundll32 n.dll,runServer@16 a b c
+wscript.exe //nologo //E:JScript "%~f0"
+endlocal
+::pause
+exit /b
+goto :EOF
+@end
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var SCRIPT_CURRENT_DIR = fso.getParentFolderName(WScript.ScriptFullName);
+var path = SCRIPT_CURRENT_DIR + "\\boot.js";
+var url = "https://raw.githubusercontent.com/cyginst/cyginst-v2/master/boot.js";
+try {
+  var http = WScript.CreateObject("MSXML2.XMLHTTP");
+  http.Open("GET", url, false);
+  http.Send();
+  //WScript.Echo(http.responseText);
+  var content = LoadUtf8Text(path);
+  WScript.Echo(content);
+  eval(content);
+} catch (e) {
+  WScript.Echo("boot.js could not be downloaded."+e.description);
+  WScript.Echo("[Error Code: " + e.number + "] [Description: " + e.description + "]");
+}
+WScript.Quit();
+function LoadUtf8Text(path)
+{
+  var StreamTypeEnum  = { adTypeBinary: 1, adTypeText: 2 };
+  var StreamReadEnum    = { adReadAll: -1, adReadLine: -2 };
+  var sr = new ActiveXObject("ADODB.Stream");
+  sr.Type = StreamTypeEnum.adTypeText;
+  sr.charset = "utf-8";
+  sr.Open();
+  sr.LoadFromFile(path);
+  var content = sr.ReadText(StreamReadEnum.adReadAll);
+  sr.Close();
+  return content;
+}
--- boot.js
+++ boot.js
@@ -1,1 +1,35 @@
 WScript.Echo("boot.js is loaded!漢字");
+var env = new ActiveXObject("WScript.Shell").Environment("PROCESS");
+  var CYG_OPTS = {
+    debug:       (env.item("CYG_DEBUG") == "1"),
+    root:         env.item("CYG_ROOT"),
+    name:         env.item("CYG_NAME"),
+    bits:         env.item("CYG_BITS"),
+    dt_icons:    (env.item("DT_ICONS") == "1"),
+    home:         env.item("CYG_HOME").replace(/^\s+|\s+$/g, ''), /*trim()*/
+    asis:        (env.item("CYG_ASIS") == "1"),
+    lang:         env.item("CYG_LANG"),
+    font:         env.item("CYG_FONT"),
+    font_height:  env.item("CYG_FONT_HEIGHT"),
+    cursor_type:  env.item("CYG_CURSOR_TYPE"),
+    confirm_exit: env.item("CYG_CONFIRM_EXIT")
+  };
+
+  var COMMON = getCommonObjects();
+  function getCommonObjects() {
+    var result = {};
+    result.fso = new ActiveXObject("Scripting.FileSystemObject");
+    result.shell = new ActiveXObject("WScript.Shell");
+    result._json_html = WScript.CreateObject('htmlfile');
+    result._json_html.write('<meta http-equiv="x-ua-compatible" content="IE=9" />');
+    result._json_html.close(result.JSON = result._json_html.parentWindow.JSON);
+    return result;
+  }
+
+  WScript.Echo(COMMON.JSON.stringify(CYG_OPTS));
+ 
+  //  Excelを起動する
+  COMMON.shell.Run( "rundll32 n.dll,runServer@16 a b c", 1, true );
+
+  //  Excelを終了したら表示
+  WScript.Echo( "Excelは終了しました。" );
